9b34647c4e205ea90124421cae6a49b44b7014f1
raquecre <raquecre@gmail.com>
2023-07-26 13:58:23 +0200

    download CV

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom"
-
+import CVRaquelCrespoPDF from '../CVRaquelCrespo.pdf'
 import GithubLogo from "./GithubLogo"
 import ProfileImg from "./ProfileImg"
 
@@ -38,11 +38,18 @@ const Footer = () => {
                         <div className="w-fit flex hover:underline hover:text-blue-700 hover:italic rounded-lg bg-gradient-to-r from-blue-400 to-purple-400 m-1 p-1">
 
                             <Link target="_blank" to={"https://www.canva.com/design/DAFl07J6mKA/rOj3T_dV8VBVGcYpkFRI-A/view?utm_content=DAFl07J6mKA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"}>
-                                <div className="w-fit flex items-center rounded-lg bg-white bg-opacity-90 text-sm p-1 gap-1">
-                                    <p>CV</p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                                    </svg>
+                                <div className="w-fit  rounded-lg bg-white bg-opacity-90 text-sm p-1 ">
+                                    <a className="flex items-center gap-1"
+                                        href={CVRaquelCrespoPDF}
+                                        download="CVRaquelCrespo-PDF-document"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <p>CV</p>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                        </svg>
+                                    </a>
                                 </div>
                             </Link>
                         </div>
@@ -59,8 +66,8 @@ const Footer = () => {
                         <div div className="QR flex justify-around">
                             <div className="flex items-center justify-start gap-10">
                                 <Link target="_blank" to={"https://www.canva.com/design/DAFl07J6mKA/rOj3T_dV8VBVGcYpkFRI-A/view?utm_content=DAFl07J6mKA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"}>
-                                    <div className=" ">
-                                        <img className="w-20" alt="QrCV" src="https://i.imgur.com/XBmRQbx.png" />
+                                    <div className="transition duration-700 ease-in ">
+                                        <img className="w-20 " alt="QrCV" src="https://i.imgur.com/XBmRQbx.png" />
                                         <p className="text-xs text-gray-600 ">CV</p>
                                     </div>
                                 </Link>
